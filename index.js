26a6cd997f78592747258a53a5213631bdf0f480
Daryn Gregory <daryn.greg@icloud.com>
2025-12-16 23:47:08 -0500

    Completed getRange

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ function getMax(numbers) {
  * @returns {number} the range of the numbers (max - min)
  */
 function getRange(numbers) {
-  // TODO
+  return getMax(numbers) - getMin(numbers);
 }
 
 /**
